33b6b9ebfa32aa6c360fd4a081cb9929dca01f32
Timon Burnos <skyficjusz@gmail.com>
2022-12-06 23:12:09 +0100

    simple demo

--- ObscurePixelsProject/main.cpp
+++ ObscurePixelsProject/main.cpp
@@ -10,23 +10,13 @@ int main()
     
     Color c(255,0,0);
     
-    ConsoleHandler console(20,10,false,true);
+    ConsoleHandler console(100,50,0,1);
 
     Clock clock;
 
     double dt = 0.0;
 
-    VertexArray line;
-    line.resize(10);
-   
-
-    for(int i = 0; i < 10;i++)
-                for(int j = 0; j < 1;j++)
-                {
-                    line[j + i * 1].bgColor = Color(rand()%128 + 128,rand()%128 + 128,rand()%128 + 128);
-                    line[j + i * 1].setPosition(Vector2f(float(0),float(i)));
-                    line[j + i * 1].setChar(' ');          
-                }
+    std::vector<VertexArray> rectangles;
 
     VertexArray rect;
     rect.resize(8);
@@ -35,25 +25,44 @@ int main()
                 for(int j = 0; j < 2;j++)
                 {
                     rect[j + i * 2].bgColor = Color(rand()%128 + 128,rand()%128 + 128,rand()%128 + 128);
-                    rect[j + i * 2].setPosition(Vector2f(float(i+5),float(j+2)));
-                    rect[j + i * 2].setChar('@');   
+                    rect[j + i * 2].fgColor = Color(rand()%128 + 128,rand()%128 + 128,rand()%128 + 128);
+                    rect[j + i * 2].setPosition(Vector2f(float(i+10),float(j+5)));
+                    rect[j + i * 2].setChar('#');   
                 }
 
+    for(int i =0;i<20;i++)  rectangles.push_back(rect);
 
+                
     while(console.isOpen())
     {
         dt += clock.getElapsedTime();
 
-        if(dt >= 5.0 / 60.0) 
+        if(dt > 5.0 / 360.0) 
         {
             // update
+            for(int x = 0 ; x < 10; x ++)
+            {
+                int randx = x*4;
+                int randy = x+x;
+                for(int i = 0; i < 4;i++)
+                {
+                    for(int j = 0; j < 2;j++)
+                    {
+                        rectangles[x][j + i * 2].bgColor = Color(rand()%128 + 128,rand()%128 + 128,rand()%128 + 128);
+                        rectangles[x][j + i * 2].fgColor = Color(rand()%128 + 128,rand()%128 + 128,rand()%128 + 128);
+                        rectangles[x][j + i * 2].setPosition(float(randx+i),float(randy+j));
+                        rectangles[x][j + i * 2].setChar('#');   
+                    }
+                }
+            }
 
             console.clear();
-            console.draw(line);
-            console.draw(rect);
+            for(int i = 0 ; i < 10; i ++)
+            {
+                console.draw(rectangles[i]);
+            }
             console.display(); 
-
-            console.moveView(Vector2i(-1,0));
+            console.moveView(Vector2i(rand()%3 -1,rand()%3 - 1));
          
             dt = 0;
         }
